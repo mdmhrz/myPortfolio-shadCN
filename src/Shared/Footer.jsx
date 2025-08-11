54e6762cc17caf8ea3077f45d05176c4c2e24dea
mdmhrz <razufreelance@gmail.com>
2025-08-12 00:44:10 +0600

    Check

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -71,7 +71,7 @@ const Footer = () => {
                     </motion.div>
                 ))}
             </div>
-
+            {/* hello */}
             <div className="container mx-auto px-4 relative z-10">
                 {/* Main content */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mb-16">
